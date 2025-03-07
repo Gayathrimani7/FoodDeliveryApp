9453e34c6d7033096f431f9ef4b6edc904bfdd55
Gayathridevi <Gayathridevi@themajesticpeople.com>
2025-03-07 16:41:16 +0530

    Add project files.

---
+++ FoodDeliveryApp/Models/ItemModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodDeliveryApp.Models
+{
+    public class ItemModel
+    {
+        [Key]
+        public int ItemId { get; set; }
+        public string? ItemName { get; set; }
+        public string? Description { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
